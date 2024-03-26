d559b12d4b70e7302484a9d44aabb06a9f997581
Willmuseve <willmuseve@gmail.com>
2024-03-26 12:20:51 -0700

    change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module uses pymongo to change names"""
+
+
+dei update_topics(mongo_collection, name, topics):
+    """
+    changes all topics of a school docment based on the name.
+    """
+    mongo_collection.update_many({ "name": name },
+            { "$set": { "topics": topics } })
